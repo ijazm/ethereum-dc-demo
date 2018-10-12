f9139f25a04290a2e0eac33a5cc6e89fab7de0f3
Mohamed Ijaz <ijaz@Mohameds-MacBook-Pro.local>
2018-10-12 15:29:21 +0530

    demo

--- web-01/src/app/scripts/index.js
+++ web-01/src/app/scripts/index.js
@@ -1,17 +1,13 @@
-
-/// Import the page's CSS. Webpack will know what to do with it.
+// Import the page's CSS. Webpack will know what to do with it.
 //  import "../stylesheets/app.css";
-
 // Import libraries we need.
 import { default as Web3 } from 'web3';
 import { default as contract } from 'truffle-contract'
-
 // Import our contract artifacts and turn them into usable abstractions.
 import TransferEther_artifacts from '../../build/contracts/TransferEther.json'
 var TransferEther = contract(TransferEther_artifacts);
 var accounts;
 var account;
-
 const App = {
     start: function () {
         // Bootstrap the contract abstraction for Use.
@@ -21,23 +17,33 @@ const App = {
                 alert("There was an error fetching your accounts.");
                 return;
             }
-
             if (accs.length == 0) {
                 alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
                 return;
             }
-
             accounts = accs;
-            console.log(accounts);
+            console.log(accs);
+            // for (var i = 0; i < accs.length; i++) {
+            //     document.getElementById('account' + i).innerHTML = accs[i];
+            // }
+            /*  */
+            var i, j = 1;
+            var text = "";
+            for (i = 0; i < accs.length; i++) {
+                text += j + '. ' + accs[i] + "<br>";
+                j++;
+            }
+            document.getElementById("demo").innerHTML = text;
+            /*  */
             account = accounts[0];
+            var a = web3.eth.accounts;
+            console.log(text);
         });
     },
-
     setStatus: function (message) {
         var status = document.getElementById("status");
         status.innerHTML = message;
     },
-
     Transfer: function () {
         var Value = parseFloat(document.getElementById("value").value);
         var From = document.getElementById("From").value;
@@ -49,42 +55,42 @@ const App = {
             var tx = e.tx;
             var gas = e.receipt.gasUsed;
             var status = e.receipt.status;
-            document.getElementById("tx").value = tx;
-            document.getElementById("gas").value = gas;
-            document.getElementById("status").value = status;
+            var blockNumber = e.receipt.blockNumber;
+            document.getElementById("tx").innerHTML = tx;
+            document.getElementById("gas").innerHTML = gas;
+            document.getElementById("status").innerHTML = status;
+            document.getElementById("blockNumber").innerHTML = blockNumber;
+            document.getElementById("transTitle").innerHTML = "Transaction hash:";
+            document.getElementById("gasTitle").innerHTML = "Gas used:";
+            document.getElementById("statusTitle").innerHTML = "Status:";
+            document.getElementById("blockNumberTitle").innerHTML = "Block Number:";
             console.log(e);
         });
     },
-
     getBalance: function () {
         var AccountAddress = document.getElementById("user").value;
         TransferEther.deployed().then(function (instance) {
             return instance.getBalance(AccountAddress)
         }).then(function (e) {
-            localStorage.setItem("payment", e);
-            var Ether = localStorage.getItem("payment");
-            document.getElementById("result").value = Ether * Math.pow(10, -18);
+            document.getElementById("result").innerHTML = e * Math.pow(10, -18);
         });
     },
-
-    fileUploadOnchange: function(){
+    fileUploadOnchange: function () {
+        var A = "vfsdghfgfjhb";
         var stringLab = document.getElementById('fileLable');
         var file = document.getElementById('file-upload').files[0];
-        App.fileUpload(file, function(hash){
-            console.log("ipfs hash", hash); 
-            document.getElementById('fileLable').innerHTML = hash; 
-            console.log(account);
+        stringLab.innerHTML = hash;
+        console.log("ipfs hash", hash);
+        App.fileUpload(file, function (hash) {
             TransferEther.deployed().then(function (instance) {
-                return instance.store(hash, {from: account});
+                return instance.store(hash, { from: account })
             }).then(function (e) {
-                console.log(e);
-            })      
+                console.log("hasshe", e);
+            })
         });
     },
-
     fileUpload: function (file, callback) {
         console.log("file details ...", file);
-        
         if (file == undefined) {
             callback("NA");
         }
@@ -94,7 +100,7 @@ const App = {
                 const buffer = Buffer.from(e.target.result);
                 var request = new XMLHttpRequest();
                 request.open('POST', "http://" + location.hostname + "/ipfsgateway/ipfs/", true);
-                //request.open('POST',"http://192.168.0.116:5001/ipfs", true);
+                //request.open('POST', "http://192.168.0.225/ipfsgateway/ipfs", true);
                 request.setRequestHeader("Content-type", "text/plain");
                 request.send(buffer);
                 request.onreadystatechange = function () {
@@ -108,42 +114,32 @@ const App = {
         }
     }
 };
-
-window.App = App;
-window.web3 = new Web3(new Web3.providers.HttpProvider('http://'+location.hostname+'/'+'jsonrpceth01'));
-console.log(web3);
-var accList = web3.eth.accounts;
-console.log(accList);
+window.web3 = new Web3(new Web3.providers.HttpProvider('http://'+location.hostname+'/'+'jsonrpceth01'))
 window.App = App;
 App.start();
-
-/*window.addEventListener('load', function () {
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://jsonrpc' ));
-    console.log(web3);
-    var accList = web3.eth.accounts;
-    console.log("accounts"+accList);
-    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
-        console.warn(
-            'Using web3 detected from external source.' +
-            ' If you find that your accounts don\'t appear or you have 0 MetaCoin,' +
-            ' ensure you\'ve configured that source properly.' +
-            ' If using MetaMask, see the following link.' +
-            ' Feel free to delete this warning. :)' +
-            ' http://truffleframework.com/tutorials/truffle-and-metamask'
-        )
-        // Use Mist/MetaMask's provider
-        window.web3 = new Web3(web3.currentProvider)
-    } else {
-        console.warn(
-            'No web3 detected. Falling back to http://127.0.0.1:7545.' +
-            ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
-            ' Consider switching to Metamask for development.' +
-            ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
-        )
-        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-        window.web3 = new Web3(new Web3.providers.HttpProvider('http://'+location.hostname+'/'+'jsonrpc'));
-    }
-
-    App.start()
-})*/
+// window.App = App;
+// window.addEventListener('load', function () {
+//     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+//     if (typeof web3 !== 'undefined') {
+//         console.warn(
+//             'Using web3 detected from external source.' +
+//             ' If you find that your accounts don\'t appear or you have 0 MetaCoin,' +
+//             ' ensure you\'ve configured that source properly.' +
+//             ' If using MetaMask, see the following link.' +
+//             ' Feel free to delete this warning. :)' +
+//             ' http://truffleframework.com/tutorials/truffle-and-metamask'
+//         )
+//         // Use Mist/MetaMask's provider
+//         window.web3 = new Web3(web3.currentProvider)
+//     } else {
+//         console.warn(
+//             'No web3 detected. Falling back to http://127.0.0.1:7545.' +
+//             ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
+//             ' Consider switching to Metamask for development.' +
+//             ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
+//         )
+//         // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+//         window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'))
+//     }
+//     App.start()
+// })
